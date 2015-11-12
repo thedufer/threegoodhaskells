230ee577c5b2756452ccd09004a401b3aa5024af
Aaron Dufour <thedufer@gmail.com>
2015-11-11 19:10:49 -0500

    update Mailgun to not cause warnings

--- Mailgun.hs
+++ Mailgun.hs
@@ -5,7 +5,7 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Control
 import Network.Mail.Mime (Mail, renderMail')
 import Network.HTTP.Client.MultipartFormData
-import Network.HTTP.Conduit (withManager, httpLbs, Response, Request(..), Manager, parseUrl, applyBasicAuth)
+import Network.HTTP.Conduit (newManager, tlsManagerSettings, httpLbs, Response, Request(..), Manager, parseUrl, applyBasicAuth)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import Data.Text
@@ -21,11 +21,10 @@ makeParts to mail = do
   let messagePart = partLBS "message" mailLBS
   return [partBS "to" (encodeUtf8 to), messagePart { partFilename = Just "message", partContentType = Just defaultMimeType }]
 
-sendMessage :: (MonadIO m, MonadBaseControl IO m, MonadThrow m) =>
-                String -> String -> String -> Mail -> m (Response LBS.ByteString)
-sendMessage domain apiKey to mail =
-  withManager $ \manager ->
-    sendWith manager domain apiKey to mail
+sendMessage :: String -> String -> String -> Mail -> IO (Response LBS.ByteString)
+sendMessage domain apiKey to mail = do
+  manager <- newManager tlsManagerSettings
+  liftIO $ sendWith manager domain apiKey to mail
 
 sendWith :: (MonadIO m, MonadBaseControl IO m, MonadThrow m) =>
             Manager -> String -> String -> String -> Mail -> m (Response LBS.ByteString)
